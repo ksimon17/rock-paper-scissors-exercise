cc3b7cd42ff1833a93a30092ded1c19c60fd7354
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-01-28 13:45:23 -0500

    Update game.py
    Updated If-else block to match python syntax and added output statements to display final results to user

--- game.py
+++ game.py
@@ -23,9 +23,9 @@
 
 
 
+import random
 
-
-
+print ("-------------------------")
 print("Welcome to the Rock, Paper, Scissors Shoot Game!")
 
 # Creating a List of Valid Choices to Use in Future Computer Simulations
@@ -35,44 +35,50 @@ listOfChoices = ["rock", "paper","scissors"]
 userSelection = input("To play the game, Please choose one of 'rock', 'paper', or 'scissors': ")
 
 #2. Validating User Input / #3 Simulating Computer Selection / #4. Determining a Winner
-if (userSelection != "rock" and userSelection != "paper" and userSelection != "scissors") {
+if (userSelection != "rock" and userSelection != "paper" and userSelection != "scissors"):
     
     print("Unfortunately, this user input is invalid. For reference, the valid"
          + "input is either 'rock', 'paper', or 'scissors'. However, you inputted" + selection 
          + ", which is not valid. To replay the game, please reinsert python game.py in"
-         + " your command line and input an appropriate selection when prompted. Thank you for your help!")
-} else {
+         + " your command line and input an appropriate selection when prompted. Thank you!")
+else :
     # Simulating Computer Selection
     computerSelection = random.choice(listOfChoices)
     
     # DETERMINING THE WINNER
     userResult = ""
-    if (userSelection == computerSelection) { # TIE
+    if (userSelection == computerSelection): # TIE
         userResult = "tie"
-    } else {
-        if (computerSelection == "rock") { # COMPUTER PICKS PAPER
-            if (userSelection == "scissors") { # scissors (user) vs rock (comp)
+    else:
+        if (computerSelection == "rock"): # COMPUTER PICKS PAPER
+            if (userSelection == "scissors"): # scissors (user) vs rock (comp)
                 userResult = "loss"
-            } else { #userSelection == "paper" -- paper (user) vs rock (comp)
+            else: #userSelection == "paper" -- paper (user) vs rock (comp)
                 userResult = "win"
-            }
-        } else if (computerSelection == "scissors") { # COMPUTER PICKS SCISSORS
-            if (userSelection == "rock") { # rock (user) vs. scissors (comp)
+        elif (computerSelection == "scissors"): # COMPUTER PICKS SCISSORS
+            if (userSelection == "rock"): # rock (user) vs. scissors (comp)
                 userResult = "win"
-            } else { #userSelection == "paper" -- paper (user) vs. scissors (comp)
+            else: #userSelection == "paper" -- paper (user) vs. scissors (comp)
                 userResult = "loss"
-            }
-        } else { # computerSelection == "paper" -- COMPUTER PICKS PAPER
-            if (userSelection == "rock") { # rock (user) vs. paper (comp)
+        else: # computerSelection == "paper" -- COMPUTER PICKS PAPER
+            if (userSelection == "rock"): # rock (user) vs. paper (comp)
                 userResult = "loss"
-            } else { #userSelection == "scissors" -- scissors (user) vs paper (comp)
+            else: #userSelection == "scissors" -- scissors (user) vs paper (comp)
                 userResult = "win"
-            }
-        }
-    }
-}
+
+#DISPLAY FINAL RESULTS 
+print ("-------------------------")
+print ("You chose: " + userSelection)
+print ("The computer chose: " + computerSelection)
+print ("-------------------------")
+if (userResult == "win"):
+    print("Congratulations! You won the game!")
+else:
+    print("Oh, the computer won. It's ok. Better luck next time.")
+print ("-------------------------")
+print ("Thanks for playing. Please play again!")
 
 
-
-print("Rock, Paper, Scissors, Shoot!")
+#initial code 
+#print("Rock, Paper, Scissors, Shoot!")
 
