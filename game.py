9bc8e072f8fd4f215c6cd2c840b2c106ba1c55f5
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-01-31 12:06:16 -0500

    Update game.py
    Updated variable names to include underscores

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ def determine_winner(choice_1, choice_2):
             winner = "scissors"
     return winner
 
-
+# MAIN 
 if __name__ == "__main__":
 
     # Set the username to the default "Player One" or to the user's provided one
@@ -61,19 +61,19 @@ if __name__ == "__main__":
     print ("-------------------------")
 
     # Create a List of Valid Choices to Use in Future Computer Simulations
-    listOfChoices = ["rock", "paper","scissors"]
+    list_of_choices = ["rock", "paper","scissors"]
 
     # User Input 
-    userSelection = input("To play the game, Please choose one of 'rock', 'paper', or 'scissors': ")
+    user_selection = input("To play the game, Please choose one of 'rock', 'paper', or 'scissors': ")
 
     # Validating User Input / Simulating Computer Selection / Determining a Winner
-    if (userSelection != "rock" and userSelection != "paper" and userSelection != "scissors"):
+    if (user_selection != "rock" and user_selection != "paper" and user_selection != "scissors"):
         #Invalid Input -- Inform the User and Gracefully Exit the Program
         print("")
         print ("-------------------------")
         print("Unfortunately, this user input is invalid.")
         print("For reference, the valid input is either 'rock', 'paper', or 'scissors'.") 
-        print("However, you inputted '" + userSelection + "', which is not valid.")
+        print("However, you inputted '" + user_selection + "', which is not valid.")
         print("To replay the game, please reinsert python game.py in your command line") 
         print("and input an appropriate selection when prompted.")
         print("Thank you!")
@@ -82,37 +82,37 @@ if __name__ == "__main__":
         exit() # gracefully exit the program
     else :
         # Simulating Computer Selection
-        computerSelection = random.choice(listOfChoices)
+        computer_selection = random.choice(list_of_choices)
         
         # Determining the Winner
-        userResult = ""
-        if (userSelection == computerSelection): # TIE
-            userResult = "tie"
+        user_result = ""
+        if (user_selection == computer_selection): # TIE
+            user_result = "tie"
         else:
-            if (computerSelection == "rock"): # COMPUTER PICKS PAPER
-                if (userSelection == "scissors"): # scissors (user) vs rock (comp)
-                    userResult = "loss"
-                else: #userSelection == "paper" -- paper (user) vs rock (comp)
-                    userResult = "win"
-            elif (computerSelection == "scissors"): # COMPUTER PICKS SCISSORS
-                if (userSelection == "rock"): # rock (user) vs. scissors (comp)
-                    userResult = "win"
-                else: #userSelection == "paper" -- paper (user) vs. scissors (comp)
-                    userResult = "loss"
-            else: # computerSelection == "paper" -- COMPUTER PICKS PAPER
-                if (userSelection == "rock"): # rock (user) vs. paper (comp)
-                    userResult = "loss"
-                else: #userSelection == "scissors" -- scissors (user) vs paper (comp)
-                    userResult = "win"
+            if (computer_selection == "rock"): # COMPUTER PICKS PAPER
+                if (user_selection == "scissors"): # scissors (user) vs rock (comp)
+                    user_result = "loss"
+                else: #user_selection == "paper" -- paper (user) vs rock (comp)
+                    user_result = "win"
+            elif (computer_selection == "scissors"): # COMPUTER PICKS SCISSORS
+                if (user_selection == "rock"): # rock (user) vs. scissors (comp)
+                    user_result = "win"
+                else: #user_selection == "paper" -- paper (user) vs. scissors (comp)
+                    user_result = "loss"
+            else: # computer_selection == "paper" -- COMPUTER PICKS PAPER
+                if (user_selection == "rock"): # rock (user) vs. paper (comp)
+                    user_result = "loss"
+                else: #user_selection == "scissors" -- scissors (user) vs paper (comp)
+                    user_result = "win"
 
     #Display Final Results 
     print ("-------------------------")
-    print ("You chose: " + userSelection)
-    print ("The computer chose: " + computerSelection)
+    print ("You chose: " + user_selection)
+    print ("The computer chose: " + computer_selection)
     print ("-------------------------")
-    if (userResult == "win"):
+    if (user_result == "win"):
         print("Congratulations! You won the game!")
-    elif (userResult == "loss"):
+    elif (user_result == "loss"):
         print("Oh, the computer won. It's ok. Better luck next time.")
     else: 
         print("You tied. Nice job!")
